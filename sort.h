d0e9193c9c6f3b8dd868fd82c41f33df6068b77f
sara-mohamd <saraamohamed0038@gmail.com>
2023-09-25 10:55:14 +0300

    task

--- sort.h
+++ sort.h
@@ -2,11 +2,35 @@
 #define SORT_H
 #include <stdlib.h>
 #include <stdio.h>
+/* Bitonic sort comparison direction macros */
+#define UP 0
+#define DOWN 1
 
-/* Sorting methods */
-void quick_sort_hoare(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
+/**
+ * enum boolean - Counting up the Boolean values.
+ * @false: makes 0.
+ * @true: Exactly 1.
+ */
+typedef enum boolean
+{
+false = 0,
+true
+} boolean;
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
+} listint_t;
+
 
 /* printing assistant features */
 void print_array(const int *array, size_t size);
