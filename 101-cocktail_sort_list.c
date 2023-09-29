01d5c6c2c509ee1720ea83a8b83ec593cb882f45
sara-mohamd <saraamohamed0038@gmail.com>
2023-09-29 22:56:12 +0300

    task

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,74 +1,74 @@
 #include "sort.h"
 
 
-void swop_node_behnnd(listint_t **list, listint_t **tail, listint_t **shoker);
+void swop_node_behnnd(listint_t **list, listint_t **lastt, listint_t **rekosh);
 void cocktail_sort_list(listint_t **list);
-void swop_node_ahood(listint_t **list, listint_t **tail, listint_t **shoker);
+void swop_node_ahood(listint_t **list, listint_t **lastt, listint_t **rekosh);
 
 /**
  * swop_node_ahood - Swap a is node Replace doubly-linked list listint_t
  *     integer list with the node before it.
  * @list: a pointer to the first integer in a list that is doubly linked.
- * @tail: a pointing device to the doubly-linked list's tail.
- * @shoker: a reference to the cocktail shoker algorithm's swapp node.
+ * @lastt: a pointing device to the doubly-linked list's lastt.
+ * @rekosh: a reference to the cocktail rekosh algorithm's swapp node.
  */
-void swop_node_ahood(listint_t **list, listint_t **tail, listint_t **shoker)
+void swop_node_ahood(listint_t **list, listint_t **lastt, listint_t **rekosh)
 {
-listint_t *temp1 = (*shoker)->next;
+listint_t *temp1 = (*rekosh)->next;
 
-if ((*shoker)->prev
+if ((*rekosh)->prev
 != NULL)
-(*shoker)->prev->next = temp1;
+(*rekosh)->prev->next = temp1;
 else
 *list = temp1;
-temp1->prev = (*shoker)->prev;
-(*shoker)->next = temp1->next;
+temp1->prev = (*rekosh)->prev;
+(*rekosh)->next = temp1->next;
 if (temp1->next != NULL)
 temp1->next->prev =
-*shoker;
+*rekosh;
 else
-*tail = *shoker;
-(*shoker)->prev = temp1;
+*lastt = *rekosh;
+(*rekosh)->prev = temp1;
 temp1->next =
-*shoker;
-*shoker = temp1;
+*rekosh;
+*rekosh = temp1;
 }
 
 /**
  * swop_node_behnnd - Swap a node Replace doubly-linked listint_t
  *       list of integers and the node that follows it.
  * @list: a pointer to the first integer in a doubly-linked list.
- * @tail: a pointing device to the doubly-linked list's tail.
- * @shoker: a reference to the cocktail shoker algorithm's curr swapp node.
+ * @lastt: a pointing device to the doubly-linked list's lastt.
+ * @rekosh: a reference to the cocktail rekosh algorithm's curr swapp node.
  */
-void swop_node_behnnd(listint_t **list, listint_t **tail, listint_t **shoker)
+void swop_node_behnnd(listint_t **list, listint_t **lastt, listint_t **rekosh)
 {
-listint_t *temp1 = (*shoker)->prev;
+listint_t *temp1 = (*rekosh)->prev;
 
-if ((*shoker)->next != NULL)
-(*shoker)->next->prev = temp1;
+if ((*rekosh)->next != NULL)
+(*rekosh)->next->prev = temp1;
 else
-*tail = temp1;
-temp1->next = (*shoker)->next;
-(*shoker)->prev = temp1->prev;
+*lastt = temp1;
+temp1->next = (*rekosh)->next;
+(*rekosh)->prev = temp1->prev;
 if (temp1->prev != NULL)
 temp1->prev->next =
-*shoker;
+*rekosh;
 else
-*list = *shoker;
-(*shoker)->next = temp1;
-temp1->prev = *shoker;
-*shoker = temp1;
+*list = *rekosh;
+(*rekosh)->next = temp1;
+temp1->prev = *rekosh;
+*rekosh = temp1;
 }
 
 /**
  * cocktail_sort_list - a doubly-linked list  integers nam listint_t sort in
- *           using the cocktail shoker algorithm to sort in ascend order.
+ *           using the cocktail rekosh algorithm to sort in ascend order.
  * @list: a pointer to the head a doubly-linked list of type listint_t.
  */
 void cocktail_sort_list(listint_t **list)
 {
-listint_t *tail, *shoker;
+listint_t *lastt, *rekosh;
 boolean shaken_not_stirred = false;
 
 if (list == NULL ||
@@ -76,36 +76,36 @@ if (list == NULL ||
 (*list)->next == NULL)
 return;
 
-tail = *list;
-while (tail->next !=
+lastt = *list;
+while (lastt->next !=
 NULL)
-tail = tail->next;
+lastt = lastt->next;
 
 while (shaken_not_stirred == false)
 {
 shaken_not_stirred = true;
-shoker = *list;
-while (shoker !=
-tail)
+rekosh = *list;
+while (rekosh !=
+lastt)
 {
-if (shoker->n > shoker->next->n)
+if (rekosh->n > rekosh->next->n)
 {
-swop_node_ahood(list, &tail, &shoker);
+swop_node_ahood(list, &lastt, &rekosh);
 print_list((const listint_t *)*list);
 shaken_not_stirred = false;
 }
-shoker = shoker->next;
+rekosh = rekosh->next;
 }
-shoker = tail->prev;
-while (shoker != *list)
+rekosh = lastt->prev;
+while (rekosh != *list)
 {
-if (shoker->n < shoker->prev->n)
+if (rekosh->n < rekosh->prev->n)
 {
-swop_node_behnnd(list, &tail, &shoker);
+swop_node_behnnd(list, &lastt, &rekosh);
 print_list((const listint_t *)*list);
 shaken_not_stirred = false;
 }
-shoker = shoker->prev;
+rekosh = rekosh->prev;
 }
 }
 }
